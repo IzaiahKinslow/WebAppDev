7e3dfe916dc5b3d81d31ac853b303eea923795c5
IzaiahKinslow <ikinslow@csi.edu>
2026-09-23 10:15:43 -0600

    Week 6

---
+++ Week6/scripts.js
@@ -0,0 +1,23 @@
+// comaprison operators
+// === equal to
+// !== not equal
+
+
+
+console.log(1===1);
+console.log(1!==1);
+
+let score=85;
+if (score>89){
+    console.log("You got an A");
+}
+else if(score>79){
+    console.log("you have a B");
+}
+else if (score>69){
+    console.log("you got a C");
+}
+else {
+    console.log("You have a mediocre grade");
+}
+
